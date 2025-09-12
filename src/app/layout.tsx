071709e30081b45317fb323a430e3f8589b556e6
scriptkid03 <edmund.phillips@acity.edu.gh>
2025-09-12 20:41:21 +0000

    feat: add mode toggle component with dropdown menu for theme selection
    - Implemented ModeToggle component to switch between light, dark, and system themes.
    - Created reusable Button component with variants and sizes.
    - Developed DropdownMenu component for theme selection with appropriate items and triggers.
    - Added Separator component for visual separation in dropdown menus.
    - Updated RootLayout to include smooth scrolling behavior.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html
+      lang='en'
+      className={"scroll-smooth"}
+      suppressHydrationWarning
+      data-scroll-behavior='smooth'
+    >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
